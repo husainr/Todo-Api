00de741efe6f6b6d1633c78df7799346c0ff7b18
Your Name <you@example.com>
2018-11-09 17:39:17 +0530

    added hash password script

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 4
- };
+var pass = '123abc';
 
-var token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(pass, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+var hashpass = '$2a$10$vGS4ihyf885tNmgPWo7C/OttyfWQq0V35x2Xxf7MVhZlzBQu4TYqi';
 
+bcrypt.compare(pass,hashpass, (err,res) => {
+  console.log(res);
+});
 
-
-
-
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// var data = {
+//   id: 4
+//  };
+//
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+//
+//
+//
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'i am user one';
 //
 // var hash = SHA256(message).toString();
